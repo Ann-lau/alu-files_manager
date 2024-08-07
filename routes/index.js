f1205b2dc059554559d857f22a62f896e9da2cc7
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2024-08-07 22:51:27 +0300

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const AppController = require('../controllers/AppController');
-
 const router = express.Router();
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
+// Existing routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
+// New route for user creation
+router.post('/users', UsersController.postNew);
+
 module.exports = router;
+
