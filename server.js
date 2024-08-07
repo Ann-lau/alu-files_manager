e22720cafabaf1dd76f70752310656f94093ab19
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2024-08-07 22:37:04 +0300

    updates

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-import express from 'express';
-import controllerRouting from './routes/index';
+const express = require('express');
+require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+const routes = require('./routes');
 
-app.use(express.json());
+app.use('/', routes);
 
-controllerRouting(app);
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-export default app;
--- utils/redis.js
+++ utils/redis.js
@@ -1,9 +1,9 @@
 const redis = require('redis');
+require('dotenv').config();
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-
     this.client.on('error', (err) => {
       console.error('Redis client error:', err);
     });
@@ -16,23 +16,17 @@ class RedisClient {
   async get(key) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (err, value) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(value);
-        }
+        if (err) return reject(err);
+        resolve(value);
       });
     });
   }
 
   async set(key, value, duration) {
     return new Promise((resolve, reject) => {
-      this.client.set(key, value, 'EX', duration, (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
+      this.client.setex(key, duration, value, (err) => {
+        if (err) return reject(err);
+        resolve();
       });
     });
   }
@@ -40,17 +34,14 @@ class RedisClient {
   async del(key) {
     return new Promise((resolve, reject) => {
       this.client.del(key, (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
+        if (err) return reject(err);
+        resolve();
       });
     });
   }
 }
 
 const redisClient = new RedisClient();
-
 module.exports = redisClient;
+
 
